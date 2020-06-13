aafff093034c214174b7df5a766a59c37a7e9b1a
Kevin Dumont <kevin.dumont@seloger.com>
2020-06-13 16:19:29 +0200

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-
 const port = 5000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
